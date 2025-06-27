f429ea0ea68665a13b40d2a3de37e000b66d4b61
Fitra Aditya <fitraditya@gmail.com>
2025-06-28 06:26:27 +0700

    Add cors.

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
 from src.routers import api
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True,
+)
 
 @app.get("/")
 async def root():
