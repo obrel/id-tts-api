3e3c6dc28950cba88666650974797ad78b406076
Fitra Aditya <fitraditya@gmail.com>
2025-06-29 09:05:36 +0700

    Add method to convert number to word.

---
+++ src/lib/num2words/num2words.py
@@ -0,0 +1,11 @@
+import re
+from num2words import num2words
+
+def convert_numbers_to_words(input_string):
+    numbers = re.findall(r'\d+', input_string)
+
+    for number in numbers:
+        word = num2words(int(number), lang="id")
+        input_string = input_string.replace(number, word)
+    
+    return input_string
